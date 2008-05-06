4368373dc77a330d955ce18965e2727493f36d4c
niklaus.lee <niklaus.lee@c3ab6631-374c-0410-bf4d-d353b85c9575>
2008-05-06 13:55:47 +0000

    remote UbiAbstractRef class

--- trunk/ubiscript/src/org/ubiscript/UbiNetRef.java
+++ trunk/ubiscript/src/org/ubiscript/UbiNetRef.java
@@ -1,28 +1,32 @@
 package org.ubiscript;
 
-public class UbiNetRef extends UbiAbstractRef {
+public class UbiNetRef extends UbiObject {
+	
 	private String location;
 	private long baseId;
+	
 	public UbiNetRef(String location, long baseId, String name) {
-		super(UbiAbstractRef.REF_BY_NAME, name, -1);
+		super(null);
 		this.location = location;
 		this.baseId = baseId;
 	}
+	
 	public UbiNetRef(String location, long baseId, int index) {
-		super(UbiAbstractRef.REF_BY_NAME, null, index);
+		super(null);
 		this.location = location;
 		this.baseId = baseId;
 	}
+	
 	public String getLocation() {
 		return location;
 	}
+	
 	public long getBaseId() {
 		return baseId;
 	}
+	
 	public String toString() {
-		String s = "<NETREF:" + location + ":" + baseId + ":"
-				+ (nameOrIndex == UbiAbstractRef.REF_BY_NAME ? name : index)
-				+ ">";
+		String s = "<NETREF:" + location + ":" + baseId + ">";
 		return s;
 	}
 }
